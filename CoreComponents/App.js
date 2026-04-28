ccbdd6287488ef3e79c5bd33640c7989d1f92ce2
TimiMaliki <malikitimi@gmail.com>
2026-04-28 16:21:53 +0100

    tutorial-scrollview

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,14 +1,18 @@
-import {View,Text,Image,ImageBackground} from "react-native";
+import {View,Text,ScrollView,ImageBackground} from "react-native";
 const Socrates = require("./assets/Pin su 风格.jpeg")
 
 
 export default function App(){
   return(
-    <View style={{flex: 1, backgroundColor: "red",  padding: "30"}}> 
-     {/* <Image source={Socrates} style={{flex:1, width: 300, height: 300}} /> */}
-     <ImageBackground source={Socrates} style={{flex:1, width: 300, height: 300}}>
+    <ScrollView style={{flex: 1, backgroundColor: "red",  padding: "30"}}> 
       <Text style={{color: "white", fontSize: 30, fontWeight: "bold"}}>Socrates</Text>
+      <Text style={{color: "white", fontSize: 30, fontWeight: "bold"}}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus cumque 
+        qui animi vitae quos perferendis similique itaque facilis suscipit fuga, obcaecati repudiandae ,
+         qui animi vitae quos perferendis similique itaque facilis suscipit fuga, obcaecati repudian
+      </Text>
+     <ImageBackground source={Socrates} style={{flex:1, width: 300, height: 500}}>
      </ImageBackground>
-    </View>
+    </ScrollView>
   )
 }
