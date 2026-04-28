63b942220a7f6fd79b7f7cf9065be3c94231116a
TimiMaliki <malikitimi@gmail.com>
2026-04-28 16:28:18 +0100

    tutorial-button

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,10 +1,11 @@
-import {View,Text,ScrollView,ImageBackground} from "react-native";
+import {View,Text,ScrollView,ImageBackground,Button} from "react-native";
 const Socrates = require("./assets/Pin su 风格.jpeg")
 
 
 export default function App(){
   return(
-    <ScrollView style={{flex: 1, backgroundColor: "red",  padding: "30"}}> 
+   <View style={{flex: 1, backgroundColor: "red",  padding: "30"}}>
+     <ScrollView> 
       <Text style={{color: "white", fontSize: 30, fontWeight: "bold"}}>Socrates</Text>
       <Text style={{color: "white", fontSize: 30, fontWeight: "bold"}}>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus cumque 
@@ -13,6 +14,8 @@ export default function App(){
       </Text>
      <ImageBackground source={Socrates} style={{flex:1, width: 300, height: 500}}>
      </ImageBackground>
+     <Button title="Click Me" onPress={()=> console.log("click")} color={"purple"}/>
     </ScrollView>
+   </View>
   )
 }
