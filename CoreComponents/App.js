57160623cf4f6ae411c34a8d615b57844ac484c2
TimiMaliki <malikitimi@gmail.com>
2026-04-28 15:41:38 +0100

    tutorial -View

---
+++ CoreComponents/App.js
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+
+export default function App(){
+  return(
+    <View style={{flex: 1, backgroundColor: "red",  }}>
+     <View style={{width: 100, height: 100, backgroundColor: "blue", padding: 20}}>
+      </View>
+    </View>
+  )
+}
