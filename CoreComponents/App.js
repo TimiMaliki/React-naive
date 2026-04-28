f4347818fe72ba5742c065ab38607e11f0cba395
TimiMaliki <malikitimi@gmail.com>
2026-04-28 17:19:16 +0100

    tutorial-modal

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,25 +1,64 @@
-import {View,Text,ScrollView,ImageBackground,Button,Pressable} from "react-native";
-const Socrates = require("./assets/Pin su 风格.jpeg")
+import {
+  View,
+  Text,
+  ScrollView,
+  ImageBackground,
+  Button,
+  Pressable,
+  Modal,
+} from "react-native";
+const Socrates = require("./assets/Pin su 风格.jpeg");
+import { useState } from "react";
 
+export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
 
-export default function App(){
-  return(
-   <View style={{flex: 1, backgroundColor: "red",  padding: "30"}}>
-     <ScrollView> 
-     <Pressable onPress={() => console.log("text pressed")}>
-       <Text style={{color: "white", fontSize: 30, fontWeight: "bold"}}>Socrates</Text>
-      <Text style={{color: "white", fontSize: 30, fontWeight: "bold"}}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus cumque 
-        qui animi vitae quos perferendis similique itaque facilis suscipit fuga, obcaecati repudiandae ,
-         qui animi vitae quos perferendis similique itaque facilis suscipit fuga, obcaecati repudian
-      </Text>
-      </Pressable>
+  return (
+    <View style={{ flex: 1, backgroundColor: "red", padding: "30" }}>
+      <ScrollView>
+          <Button
+          title="Modal"
+          onPress={() => setModalVisible(true)}
+          color={"purple"}
+        />
+        <Modal
+          visible={modalVisible}
+        >
+          <View style={{backgroundColor: "white", padding: "30" }}>
+            <Text style={{ color: "black", fontSize: 30, fontWeight: "bold" }}>
+              Modal
+            </Text>
+            <Button
+              title="Close me"
+              onPress={() => setModalVisible(false)}
+              color={"blue"}
+            />
+          </View>
+        </Modal>
+        <Pressable onPress={() => console.log("text pressed")}>
+          <Text style={{ color: "white", fontSize: 30, fontWeight: "bold" }}>
+            Socrates
+          </Text>
+          <Text style={{ color: "white", fontSize: 30, fontWeight: "bold" }}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+            cumque qui animi vitae quos perferendis similique itaque facilis
+            suscipit fuga, obcaecati repudiandae , qui animi vitae quos
+            perferendis similique itaque facilis suscipit fuga, obcaecati
+            repudian
+          </Text>
+        </Pressable>
         <Pressable onPressIn={() => console.log("image pressed")}>
-     <ImageBackground source={Socrates} style={{flex:1, width: 300, height: 500}}>
-     </ImageBackground>
-     </Pressable>
-     <Button title="Click Me" onPress={()=> console.log("click")} color={"purple"}/>
-    </ScrollView>
-   </View>
-  )
+          <ImageBackground
+            source={Socrates}
+            style={{ flex: 1, width: 300, height: 500 }}
+          ></ImageBackground>
+        </Pressable>
+        <Button
+          title="Click Me"
+          onPress={() => console.log("click")}
+          color={"purple"}
+        />
+      </ScrollView>
+    </View>
+  );
 }
