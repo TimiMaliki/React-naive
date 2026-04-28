4a79943e35a8bedced1b487aa6ff05680f23fadd
TimiMaliki <malikitimi@gmail.com>
2026-04-28 17:03:52 +0100

    tutorial-pressable

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,4 +1,4 @@
-import {View,Text,ScrollView,ImageBackground,Button} from "react-native";
+import {View,Text,ScrollView,ImageBackground,Button,Pressable} from "react-native";
 const Socrates = require("./assets/Pin su 风格.jpeg")
 
 
@@ -6,14 +6,18 @@ export default function App(){
   return(
    <View style={{flex: 1, backgroundColor: "red",  padding: "30"}}>
      <ScrollView> 
-      <Text style={{color: "white", fontSize: 30, fontWeight: "bold"}}>Socrates</Text>
+     <Pressable onPress={() => console.log("text pressed")}>
+       <Text style={{color: "white", fontSize: 30, fontWeight: "bold"}}>Socrates</Text>
       <Text style={{color: "white", fontSize: 30, fontWeight: "bold"}}>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus cumque 
         qui animi vitae quos perferendis similique itaque facilis suscipit fuga, obcaecati repudiandae ,
          qui animi vitae quos perferendis similique itaque facilis suscipit fuga, obcaecati repudian
       </Text>
+      </Pressable>
+        <Pressable onPress={() => console.log("image pressed")}>
      <ImageBackground source={Socrates} style={{flex:1, width: 300, height: 500}}>
      </ImageBackground>
+     </Pressable>
      <Button title="Click Me" onPress={()=> console.log("click")} color={"purple"}/>
     </ScrollView>
    </View>
