21f7b709927589d9f2e5e6880b242e8bf0b53f89
TimiMaliki <malikitimi@gmail.com>
2026-04-30 16:57:23 +0100

    tutotial : flex-wrap

--- Tutorial/CoreComponents/App.js
+++ Tutorial/CoreComponents/App.js
@@ -4,7 +4,9 @@ import Box from "./component/Box";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
+    flexWrap: "nowrap",
+    height: 500,
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
@@ -22,10 +24,10 @@ export default function App() {
       <Box style={{ backgroundColor: "green",}}>  Box 1 </Box>
       <Box style={{ backgroundColor: "black",}}> Box 2</Box>
       <Box style={{ backgroundColor: "red",}}> Box 3 </Box>
-      <Box style={{ backgroundColor: "green" }}> Box 4 </Box>
-      <Box style={{ backgroundColor: "yellow" }}> Box 5 </Box>
-      <Box style={{ backgroundColor: "lemonchiffon" }}> Box 6 </Box>
-      <Box style={{ backgroundColor: "skyblue" }}> Box 7 </Box>
+      <Box style={{ backgroundColor: "green"}}> Box 4 </Box>
+      <Box style={{ backgroundColor: "yellow"}}> Box 5 </Box>
+      <Box style={{ backgroundColor: "lemonchiffon"}}> Box 6 </Box>
+      <Box style={{ backgroundColor: "skyblue"}}> Box 7 </Box>
     </View>
   );
 }
