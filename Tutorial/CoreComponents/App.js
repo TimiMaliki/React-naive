472ef5b9f3998fcc2e89c617ca0ae804b13d3356
TimiMaliki <malikitimi@gmail.com>
2026-04-30 16:30:26 +0100

    tutotial : align self

--- Tutorial/CoreComponents/App.js
+++ Tutorial/CoreComponents/App.js
@@ -8,14 +8,15 @@ import Box from "./component/Box";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    alignSelf: "stretch",
+    alignItems: "flex-end",
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
   },
   text:{
-    color:"white"
+    color:"white",
+    fontSize:40,
+    fontWeight:"bold",
   }
 
 })
@@ -23,14 +24,14 @@ const styles = StyleSheet.create({
 export default function App() {
  
   return (
-    <View style={styles.container}>
-    <Box style={{backgroundColor:"green",color:"white",paddingVertical:100}}>Box 1</Box>
-     <Box style={{backgroundColor:"black",color:"white",}}> Box 2 </Box>
-      <Box style={{backgroundColor:"red",}}> Box 3 </Box>
-       {/* <Box style={{backgroundColor:"green",}}> Box 4 </Box>
-       <Box style={{backgroundColor:"yellow", }}> Box 5 </Box>
-      <Box style={{backgroundColor:"lemonchiffon", }}> Box 6 </Box>
-       <Box style={{backgroundColor:"skyblue", }}> Box 7 </Box> */}
+    <View style={[styles.text,styles.container]}>
+    <Box style={{backgroundColor:"green", alignSelf: "flex-end",}}>Box 1</Box>
+     <Box style={{backgroundColor:"black",alignSelf: "flex-start",}}> Box 2 </Box>
+      <Box style={{backgroundColor:"red",alignSelf: "center"}}> Box 3 </Box>
+       <Box style={{backgroundColor:"green",}}> Box 4 </Box>
+       <Box style={{backgroundColor:"yellow",}}> Box 5 </Box>
+      <Box style={{backgroundColor:"lemonchiffon",}}> Box 6 </Box>
+       <Box style={{backgroundColor:"skyblue",}}> Box 7 </Box>
     </View>
   );
 }
