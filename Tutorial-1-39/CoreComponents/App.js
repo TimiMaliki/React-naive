8417049915d77408c7adb240bc08a89a20cc1387
TimiMaliki <malikitimi@gmail.com>
2026-05-05 11:58:11 +0100

    added: flexShrink

---
+++ Tutorial-1-39/CoreComponents/App.js
@@ -0,0 +1,34 @@
+import { StyleSheet, View } from "react-native";
+
+import Box from "./component/Box";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 300,
+    flexDirection: "column",
+    alignContent: "flex-start",
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: "red",
+  },
+  text: {
+    color: "white",
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+});
+
+export default function App() {
+  return (
+    <View style={[styles.text, styles.container]}>
+      <Box style={{ backgroundColor: "green", top: 75,left:75 }} Children="Box 1" />
+      <Box style={{ backgroundColor: "black", }} Children="Box 2" />
+      <Box style={{ backgroundColor: "red",}} Children="Box 3" />
+      <Box style={{ backgroundColor: "green", top: 75,left:75  }} Children="Box 4" />
+      <Box style={{ backgroundColor: "yellow",position: "absolute", top: 150, left: 150 }} Children="Box 5" />
+      {/* <Box style={{ backgroundColor: "lemonchiffon" }} Children="Box 6" />
+      <Box style={{ backgroundColor: "skyblue" }} Children="Box 7" /> */}
+    </View>
+  );
+}
