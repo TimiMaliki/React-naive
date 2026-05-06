7f56086312f08055d0e8a6988a8174a63584351a
TimiMaliki <malikitimi@gmail.com>
2026-05-06 17:07:58 +0100

    Dimension Api Drawback

--- my-new-app/src/app/index.tsx
+++ my-new-app/src/app/index.tsx
@@ -1,41 +1,61 @@
-import * as Device from 'expo-device';
-import {StyleSheet ,Text , View, Dimensions} from 'react-native';
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
-const windowWidth = Dimensions.get("window").width
-const windowHeight = Dimensions.get("window").height
+
+const [dimensions, setDimensions] = useState({
+  window: Dimensions.get("window"),
+});
+
+useEffect(() => {
+  const subscription = Dimensions.addEventListener("change", ({ window }) => {
+    setDimensions({ window });
+  });
+  return () => subscription?.remove();
+});
+
+const { window } = dimensions;
+const windowWidth = window.width;
+const windowHeight = window.height;
+
+// const windowWidth = Dimensions.get("window").width
+// const windowHeight = Dimensions.get("window").height
 const styles = StyleSheet.create({
-  container : {
+  container: {
     flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
   },
   box: {
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
-    width: windowWidth > 500 ? "70%" : "90%",
-    height: windowHeight > 600 ? "60%" : "90%",
-    borderRadius: 10
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHeight > 600 ? "60%" : "90%",
+    borderRadius: 10,
   },
-  text:{
-    fontSize: 20
-  }
-
-})
-
-
-
+  text: {
+    // fontSize:  windowWidth > 500 ? 20 : 16,
+  },
+});
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-  <View style={styles.box}>
-    <Text style={styles.text}>Hello Timi</Text>
-  </View>
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 600 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text style={{ fontSize: windowWidth > 500 ? 20 : 16 }}>
+          Hello Timi
+        </Text>
+      </View>
     </View>
-  ); 
+  );
 }
-
-
