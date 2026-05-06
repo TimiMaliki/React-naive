1b88a6fcfcc5f3b662b9300a57acd534b39a1f4e
TimiMaliki <malikitimi@gmail.com>
2026-05-06 17:37:14 +0100

    Dimension Api Drawback

--- my-new-app/src/app/index.tsx
+++ my-new-app/src/app/index.tsx
@@ -1,11 +1,26 @@
-import { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+// import { useState, useEffect } from "react";
+import { StyleSheet, Text, View, useWindowDimensions} from "react-native";
 
 
+//  const [dimensions, setDimensions] = useState({
+//   window: Dimensions.get("window"),
+// });
 
+// useEffect(() => {
+//   const subscription = Dimensions.addEventListener("change", ({ window }) => {
+//     setDimensions({ window });
+//   });
+//   return () => subscription?.remove();
+// },[]);
+
+// const { window } = dimensions;
+// const windowWidth = window.width;
+// const windowHeight = window.height;
 
 // const windowWidth = Dimensions.get("window").width
 // const windowHeight = Dimensions.get("window").height
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -28,29 +43,16 @@ const styles = StyleSheet.create({
 });
 
 export default function HomeScreen() {
-  const [dimensions, setDimensions] = useState({
-  window: Dimensions.get("window"),
-});
-
-useEffect(() => {
-  const subscription = Dimensions.addEventListener("change", ({ window }) => {
-    setDimensions({ window });
-  });
-  return () => subscription?.remove();
-},[]);
-
-const { window } = dimensions;
-const windowWidth = window.width;
-const windowHeight = window.height;
+ const windowWidth = useWindowDimensions().width
+const windowHeight = useWindowDimensions().height
   return (
     <View style={styles.container}>
       <View
         style={[
           styles.box,
-          {
-            width: windowWidth > 500 ? "70%" : "90%",
-            height: windowHeight > 600 ? "60%" : "90%",
-          },
+        {    width: windowWidth > 500 ? "100%" : "90%",
+              height: windowHeight > 600 ? "60%" : "90%",
+        }
         ]}
       >
         <Text style={{ fontSize: windowWidth > 500 ? 20 : 16 }}>
