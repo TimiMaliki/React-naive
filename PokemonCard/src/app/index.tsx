a532c85424c619fdb51c523cc1f296b313f2b41a
TimiMaliki <malikitimi@gmail.com>
2026-05-11 15:44:50 +0100

    Pokemon Card

--- PokemonCard/src/app/index.tsx
+++ PokemonCard/src/app/index.tsx
@@ -7,7 +7,7 @@ import PokemonCard from '../components/PokemonComponents/PokemonCard';
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 70,
+    padding: 0,
     paddingTop:Platform.OS === "android" ? 25 : 0,
     backgroundColor: '#fff',
   },
@@ -20,11 +20,20 @@ const style = StyleSheet.create({
 
 
 export default function HomeScreen() {
+
+  const CharmanderData = {
+    name:"Charmander",
+   image: require("../../assets/images/pokemon/1.jpg"),
+    type:"Fire",
+    hp:39,
+    moves:["scratch","Ember","Growl","Leer"],
+    weaknesses:["Water","Rock"]
+  };
   return (
    <SafeAreaView style={style.container}>
      <View>
-      <Text style={style.text}>Home</Text>
-      <PokemonCard />
+      <Text style={style.text}>Pokemon</Text>
+      <PokemonCard CharmanderData={CharmanderData} />
     </View>
    </SafeAreaView>
 
--- PokemonCard/src/components/PokemonComponents/PokemonCard.js
+++ PokemonCard/src/components/PokemonComponents/PokemonCard.js
@@ -1,32 +1,56 @@
-import { StyleSheet, View, Text, Platform } from 'react-native'
+import { StyleSheet, View, Text, Platform, Image } from "react-native";
 
 const styles = StyleSheet.create({
-    card:{
-        backgroundColor: "white",
-        borderRadius:16,
-        borderWidth:2,
-        padding:16,
-        margin:16,
-        ...Platform.select({
-            ios:{
-                shadowOffset:{width:2,height:2},
-                shadowColor:"#000",
-                shadowOpacity:0.3,
-                shadowRadius:4,
-            },
-            android:{
-               evevation:5,
-            }
-        })
-    }
-})
+  card: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "#000",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        evevation: 5,
+      },
+    }),
+  },
 
-const PokemonCard = () => {
+  imageWidth:{
+    width:300,
+    height:300,
+  }
+});
+
+const PokemonCard = ({ CharmanderData }) => {
   return (
-  <View style={styles.card}>
-    <Text>Pokemon Card</Text>
-  </View>
-  )
-}
+    <View style={styles.card}>
+      <View>
+        <Text>{CharmanderData.name}</Text>
+        <Text>{CharmanderData.hp}</Text>
+      </View>
+      <Image
+        source={CharmanderData.image}
+        accessibilityLabel={`${CharmanderData.name} Pokemon`}
+        style={styles.imageWidth}
+      />
 
-export default PokemonCard
+      <View>
+        <Text>{CharmanderData.type}</Text>
+      </View>
+
+      <View>
+        <Text>Moves: {CharmanderData.moves.join(", ")}</Text>
+      </View>
+      <View>
+        <Text>Weaknesses: {CharmanderData.weaknesses.join(", ")}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default PokemonCard;
