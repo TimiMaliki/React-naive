efc1f89b8993952eff8006260391198d5e846398
TimiMaliki <malikitimi@gmail.com>
2026-05-12 06:55:32 +0100

    RNList

--- RNList/src/app/index.tsx
+++ RNList/src/app/index.tsx
@@ -1,15 +1,49 @@
-import { Platform, StyleSheet, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {StyleSheet, View,Text,ScrollView,StatusBar } from 'react-native';
+import pokemonList from "./data.json"
+import { SafeAreaView} from 'react-native-safe-area-context';
 
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+    paddingTop:StatusBar.currentHeight 
+  },
+  scrollView: {
+    paddingHorizontal: 20,
+  }
+}
 
+)
 
 export default function HomeScreen() {
   return (
-    
-    <View>
-
+         <SafeAreaView style={styles.container}>
+          <ScrollView style={styles.scrollView}>
+     <View >
+        <Text>Pokemon List</Text>
     </View>
+
+    <View>
+      {
+        pokemonList.map((pokemon) => {
+           return(
+           <View key={pokemon.id}>
+             <Text>
+            {pokemon.name}
+           </Text>
+             <Text>
+            {pokemon.type}
+           </Text>
+           </View>
+           )
+})
+      }
+    </View>
+     </ScrollView>
+   </SafeAreaView>
+   
   );
 }
 
