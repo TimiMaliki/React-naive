c31507f9c9e36bf398233dfeb29baee01e59da3c
TimiMaliki <malikitimi@gmail.com>
2026-05-16 02:43:55 +0100

    RNNETWORKING

--- RNForms/src/app/index.tsx
+++ RNForms/src/app/index.tsx
@@ -1,4 +1,12 @@
-import { StyleSheet, TextInput, StatusBar, Text } from "react-native";
+import {
+  StyleSheet,
+  TextInput,
+  StatusBar,
+  Text,
+  Switch,
+  View,
+  Button
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useState } from "react";
 
@@ -6,47 +14,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    padding: 20,
-    paddingTop: StatusBar.currentHeight,
-  },
-  scrollView: {
     paddingHorizontal: 20,
+    // paddingTop: StatusBar.currentHeight,
   },
-  card: {
-    backgroundColor: "#f0f0f0",
-    borderRadius: 10,
-    padding: 15,
-    // marginBottom: 10,
-    borderWidth: 1,
-  },
-  Text: {
-    fontSize: 22,
-    color: "#000",
-  },
-  textInput: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-  },
+
 });
 
 export default function HomeScreen() {
-  const [text, setText] = useState("");
+  
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput
-        style={styles.textInput}
-        value={text}
-        onChangeText={setText}
-        placeholder="email@gmail.com"
-        secureTextEntry={true}
-        keyboardType="numeric"
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
-      <Text style={styles.Text}>My Name is {text}</Text>
+  
     </SafeAreaView>
   );
 }
+
+
+ 
